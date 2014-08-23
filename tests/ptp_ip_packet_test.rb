a4e074e29db6d0122fa3c02af6f344b5a2ee77d0
Reiji Tokuda <stoikheia@gmail.com>
2014-08-23 16:52:29 +0900

    Added Test for event packet.

--- tests/ptp_ip_packet_test.rb
+++ tests/ptp_ip_packet_test.rb
@@ -1,7 +1,8 @@
 #!/opt/local/bin/ruby
 
 require 'test/unit'
-require '/Users/administrator/Programing/MTP/ptp_test/ptp_ip_packet.rb'
+require '../ptp.rb'
+require '../ptp_ip.rb'
 
 
 class PTP_IP_packet_test < Test::Unit::TestCase
@@ -20,7 +21,7 @@ class PTP_IP_packet_test < Test::Unit::TestCase
                     0,105,0,55,0,0,0,
                 0,0,1,0]
     
-        packet = PTP_IP_packet.new(data)
+        packet = PTPIP_packet.new(data)
         
         assert_equal(Fixnum, packet.length.class)
         assert_equal(58, packet.length)
@@ -35,7 +36,7 @@ class PTP_IP_packet_test < Test::Unit::TestCase
             150,50,152,19,196,0,65,50], payload.guid)
             
         assert_equal(String, payload.friendly_name.class)
-        assert_equal("MacBook-Air-i7\000", payload.friendly_name)
+        assert_equal("MacBook-Air-i7", payload.friendly_name)
             
         assert_equal(Fixnum, payload.protocol_version.class)
         assert_equal(65536, payload.protocol_version)
@@ -50,7 +51,7 @@ class PTP_IP_packet_test < Test::Unit::TestCase
     
         connection_number = 1
         
-        payload = PTP_IP_payload_INIT_CMD_ACK.new()
+        payload = PTPIP_payload_INIT_CMD_ACK.new()
         
         payload.conn_number = connection_number
         payload.guid = RESPONDER_GUID
@@ -66,7 +67,7 @@ class PTP_IP_packet_test < Test::Unit::TestCase
         
         assert_equal(payload_check_data, payload.to_data)
         
-        packet = PTP_IP_packet.create(payload)
+        packet = PTPIP_packet.create(payload)
         
         packet_check_data = [70, 0, 0, 0,
             2, 0, 0, 0,
@@ -81,4 +82,31 @@ class PTP_IP_packet_test < Test::Unit::TestCase
         
     end
 
+    def test_parse_event_packet_001
+        data = [18,0,0,0,
+                8,0,0,0,
+                0x02,0x40,
+                1,0,0,0,
+                1,0,8,0]
+
+        packet = PTPIP_packet.new(data)
+        
+        assert_equal(Fixnum, packet.length.class)
+        assert_equal(18, packet.length)
+        
+        payload = packet.payload
+        
+        assert_equal(Fixnum, payload.type.class)
+        assert_equal(PTPIP_PT_EventPacket, payload.type)
+        
+        assert_equal(Fixnum, payload.event_code.class)
+        assert_equal(PTP_EC_ObjectAdded, payload.event_code)
+
+        assert_equal(Fixnum, payload.transaction_id.class)
+        assert_equal(1, payload.transaction_id)
+        
+        assert_equal(1, payload.parameters.length)
+        assert_equal(Fixnum, payload.parameters[0].class)
+        assert_equal(0x080001, payload.parameters[0])
+    end
 end
